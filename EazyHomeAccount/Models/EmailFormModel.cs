7130249a0555df2a08252a1e51fd1d99ecd8153a
Saraw <Sarawoot@gmail.com>
2018-10-29 11:41:22 +0700

    reomve  Index-Card's sub header

---
+++ EazyHomeAccount/Models/EmailFormModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace EazyHomeAccount.Models
+{
+    public class EmailFormModel
+    {
+        [Required, Display(Name ="Your name")]
+        public string FromName { get; set; }
+        [Required, Display(Name = "Your email"), EmailAddress]
+        public string FromEmail { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
